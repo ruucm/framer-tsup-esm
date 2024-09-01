cef98888653e7366cad4e510cf7c0fdfc955e0b9
deploy <deploy@users.noreply.github.com>
2024-09-01 16:57:41 +0000

    Updated

---
+++ framer-tsup-esm@0.0.1/index.d.ts
@@ -0,0 +1,5 @@
+declare const hey = "2";
+
+declare function Box(): any;
+
+export { Box, hey };
---
+++ framer-tsup-esm@0.0.1/index.mjs
@@ -0,0 +1,1 @@
+var r="2";import{jsx as o}from"react/jsx-runtime";function e(){return o("div",{style:{width:100,height:100,backgroundColor:"red"},children:"Box"})}export{e as Box,r as hey};
